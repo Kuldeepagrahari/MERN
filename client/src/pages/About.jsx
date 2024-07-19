c51305119db4718ae06c5b87d90ba095e8760ef7
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-07-20 01:30:25 +0530

    chal raha , sone jaa raha hu sam

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,26 @@ import React from 'react'
 const About = () => {
   return (
     <div className='about'>
+     <div className="about-left">
+          <div className="about-my-info">
+            <h1>About Me</h1>
+          </div>
+          <div className="skills">
+            <h1>My Skills</h1>
+          </div>
+          <div className="achievments">
+              <h1>Achievments</h1>
+          </div>
+          <div className="links">
+            <h1>Links</h1>
+          </div>
+     </div>
+     <div className="about-right">
+       
+     </div>
      
-      
+
+    
     </div>
   )
 }
